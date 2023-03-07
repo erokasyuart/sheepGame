fa0cb4f5f463e2b996433ca2a5b887866884fc09
erokasyuart <erikat.stuart@gmail.com>
2023-03-08 09:49:13 +1300

    Added enum for passing data

---
+++ Assets/RW/Scripts/Shared/GameSettings.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//static class holds data across scenes
+public static class GameSettings
+{
+    //the enum
+    public static HayMachineColour hayMachineColour = HayMachineColour.Blue; //default colour is blue
+}
---
+++ Assets/RW/Scripts/Shared/HayMachineColour.cs
@@ -0,0 +1,4 @@
+public enum HayMachineColour
+{
+    Blue, Yellow, Red
+}
