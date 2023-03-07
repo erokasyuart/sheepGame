8a28ebe1e99a93f3a72a8f82e0ec817a79a08aca
erokasyuart <erikat.stuart@gmail.com>
2023-03-08 08:54:22 +1300

    Title screen buttons have function; Buttons have hover effect

---
+++ Assets/RW/Scripts/Title/ChangeColourOnMouseOver.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+//entering and exiting the area
+public class ChangeColourOnMouseOver : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public MeshRenderer model;
+    public Color normalColour;
+    public Color hoverColour;
+    // Start is called before the first frame update
+    void Start()
+    {
+        model.material.color = normalColour;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        model.material.color = hoverColour; //entering the collider, the material changes colour
+    }
+    
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        model.material.color = normalColour; //leaving the collider, material returns to normal
+    }
+}
